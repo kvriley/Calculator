772f93ae456d35e0c0c327d09d65337bd67444a7
unknown <kvriley2013@outlook.com>
2015-08-30 13:49:43 -0400

    Initial commit

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Calculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public PartialViewResult ShowCalculator()
+        {
+            var cal = new CalculatorModel();
+
+            return PartialView(cal);
+        }
+    }
+}
---
+++ Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public string[] Keys {
+            get { return new string[] { "9", "8", "7", "6", "5", "4", "3", "2", "1", "0" }; }
+        }
+
+        public CalculatorModel()
+        {
+
+        }
+    }
+}
